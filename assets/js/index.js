56dffd781439994ce25694cffbdc5b56ce2be9f3
TheKokoDiaz <wii.javd@gmail.com>
2025-07-10 19:13:12 -0600

    feat/posterDescription

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,21 +13,21 @@ poster1 = new poster({
     title: "Kittens",
     owners: "Fernando, Peter & Ivan",
     image: "assets/images/poster1.jpg",
-    info: ""
+    info: "Please, take care of them!"
 });
 
 poster2 = new poster({
-    title: "Greener World",
-    owners: "Edgar & Zoe",
+    title: "A Greener Possibility",
+    owners: "Sebastián, Edgar & Zoe",
     image: "assets/images/poster2.png",
-    info: ""
+    info: "Represents the contrast between nature and a neglected field, using a single bright and leafy tree symbolizing hope."
 });
 
 poster3 = new poster({
-    title: "Respect",
-    owners: "Edgar & Zoe",
+    title: "Nature's Silent Request",
+    owners: "Sebastián, Edgar & Zoe",
     image: "assets/images/poster3.jpg",
-    info: ""
+    info: "It reminds us that nature’s beauty doesn't ask for much—only our respect. "
 });
 
 poster4 = new poster({
@@ -44,6 +44,20 @@ poster5 = new poster({
     info: "Some turtles finding some trash in the ocean"
 });
 
+poster6 = new poster({
+    title: "Angry Seal",
+    owners: "Vicoddini",
+    image: "assets/images/poster6.png",
+    info: "Poor seal"
+});
+
+poster7 = new poster({
+    title: "Every Piece Counts",
+    owners: "Jimena, Bryan & Julio",
+    image: "assets/images/poster7.jpg",
+    info: "It highlights the environmental consequences of littering on polar wildlife. It features a family of sad penguins stranded on a shrinking ice block, surrounded by floating garbage."
+});
+
 /* posterX = new poster({
     title: "",
     owners: "",
@@ -51,7 +65,7 @@ poster5 = new poster({
     info: ""
 }); */
 
-imageArray = [poster1, poster2, poster3, poster4, poster5];
+imageArray = [poster1, poster2, poster3, poster4, poster5, poster6, poster7];
 
 //! HTML Elements
 const headerTitle = document.getElementById("headerTitle");
@@ -60,6 +74,9 @@ const headerOwners = document.getElementById("headerOwners");
 const reel = document.getElementById("reel");
 
 const slide = document.getElementById("slide");
+
+const infoButton = document.getElementById("infoButton");
+const info = document.getElementById("info");
 
 //! Functions
 function drawPosters(){
@@ -71,13 +88,40 @@ function drawPosters(){
     });
 }
 
+function toggleInfo(action){
+    if(action){
+        info.style.bottom = "0%";
+        toggleInfoButton(false);
+    }
+    
+    if(!action){
+        info.style.bottom = "-20%";
+        toggleInfoButton(true);
+    }
+
+}
+
+function toggleInfoButton(action){
+    if(action){
+        infoButton.style.bottom = "0.25rem";
+    }
+    
+    if(!action){
+        infoButton.style.bottom = "-4rem";
+    }
+}
+
 function focusPoster(poster, index){
     headerTitle.innerText = imageArray[index].title;
     headerOwners.innerText = '"' + imageArray[index].owners + '"';
 
     poster.scrollIntoView({ behavior: 'smooth', block: 'center', inline: 'center' });
     
+    toggleInfoButton(true);
+
     slide.innerText = "Poster " + (index + 1) + " of " + imageArray.length;
+
+    info.innerText = imageArray[index].info;
 }
 
 function advice(){
